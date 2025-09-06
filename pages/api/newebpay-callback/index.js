6b01517267b3e68d1377c0b43284c17a5c9ffc54
bob1127 <61307295+bob1127@users.noreply.github.com>
2025-09-06 21:06:51 +0800

    erterger

---
+++ pages/api/newebpay-callback/index.js
@@ -0,0 +1,2 @@
+export { default } from "../newebpay-callback";
+export { config } from "../newebpay-callback";
---
+++ pages/api/newebpay-notify/index.js
@@ -0,0 +1,2 @@
+export { default } from "../newebpay-notify";
+export { config } from "../newebpay-notify";
